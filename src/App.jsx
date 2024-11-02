fa42ec901675b5bcb546c12643e3985326bd534a
GrakePCH <grakepch@gmail.com>
2024-11-02 02:03:02 -0400

    update UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import dict from "./assets/dict.json";
+import Icon from "@mdi/react";
+import { mdiContentCopy, mdiFire } from "@mdi/js";
+
 
 function App() {
   const [code, setCode] = useState("");
@@ -40,14 +43,15 @@ function App() {
         <textarea id="input-chinese" name="input-chinese" rows="5" onChange={handleInputUpdate} />
         <div className="control-area">
           <button id="btn-copy" onClick={copyToClipboard}>
-            复制结果
+           <Icon path={mdiContentCopy} size={1} /> 复制结果
           </button>
-          <div>
-            <label htmlFor="toggle-fast-mode">键盘侠模式</label>
+          <div id="control-fast-mode">
+            <label htmlFor="toggle-fast-mode"><Icon path={mdiFire} size={"1.5rem"} /> 键盘侠模式</label>
             <input type="checkbox" id="toggle-fast-mode" onChange={(e) => setFastMode(e.target.checked)} />
           </div>
         </div>
         <textarea id="ouput-code" name="output-code" rows="5" disabled value={code} />
+        <p className="descrip">键盘侠模式：输入原文后无需点击“复制结果”按钮，结果会自动复制到剪贴板。</p>
       </div>
     </>
   );
